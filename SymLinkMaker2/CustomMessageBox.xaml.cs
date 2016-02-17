baa347e2995efdf447014bce16fdd5189d901465
Michael Delpach <mcored@gmail.com>
2016-02-17 21:24:12 +0800

    Ported functions from WinForms
    Utilize CustomMessageBox

---
+++ SymLinkMaker2/CustomMessageBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SymbolicLinkMaker
+{
+    public partial class CustomMessageBox : UserControl
+    {
+        public CustomMessageBox(string text, string button1 = "Ok", string button2 = "", string button3 = "")
+        {
+            InitializeComponent();
+            Text.Text = text;
+            Button1.Content = button1;
+
+            if (!string.IsNullOrEmpty(button2))
+            {
+                Button2.Visibility = Visibility.Visible;
+                Button2.Content = button2;
+            }
+
+            Button2.Content = button2;
+            if (!string.IsNullOrEmpty(button3))
+            {
+                Button3.Visibility = Visibility.Visible;
+                Button3.Content = button3;
+            }
+        }
+    }
+}
--- SymLinkMaker2/MainWindow.xaml.cs
+++ SymLinkMaker2/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gat.Controls;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SymbolicLinkMaker.Wpf
 {
@@ -25,27 +18,246 @@ namespace SymbolicLinkMaker.Wpf
             InitializeComponent();
         }
 
-        private void Window_Drop(object sender, DragEventArgs e)
+        #region Methods
+
+        private OpenDialogViewModel BrowseFolder(string title = "Browse folder")
         {
-            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+            OpenDialogView openDialog = new OpenDialogView();
+            OpenDialogViewModel vm = (OpenDialogViewModel)openDialog.DataContext;
+            vm.IsDirectoryChooser = true;
+            vm.Caption = "Browse for the link directory";
+            vm.DateFormat = OpenDialogViewModelBase.ISO8601_DateFormat;
+            vm.OpenText = "Select folder";
+            vm.SelectFolder = true;
+            vm.Owner = this;
+            vm.StartupLocation = WindowStartupLocation.CenterScreen;
+
+            return vm;
         }
 
-        private void btnBrowseLinkDir_Click(object sender, RoutedEventArgs e)
+        public async void CreateSymLink(string linkDir, string linkName, string targetDir)
         {
+            // TODO: MessageBoxStyle.Error etc.
+            // error handling
+            if (string.IsNullOrEmpty(linkDir))
+            {
+                CustomMessageBox messageBox = new CustomMessageBox("Please input a location where you want to make the Symbolic Link.");
+                await DialogHost.Show(messageBox);
+                txtLinkDir.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(linkName))
+            {
+                CustomMessageBox messageBox = new CustomMessageBox("Please input a name for the Symbolic Link.");
+                await DialogHost.Show(messageBox);
+                txtLinkName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(targetDir))
+            {
+                CustomMessageBox messageBox = new CustomMessageBox("Please input a valid target for the Symbolic Link..");
+                await DialogHost.Show(messageBox);
+                txtTargetDir.Focus();
+                return;
+            }
+
+            if (!Directory.Exists(Path.Combine(linkDir, linkName)))
+            {
+                CustomMessageBox messageBox = new CustomMessageBox("Please input a Symbolic Link with a valid path.");
+                await DialogHost.Show(messageBox);
+                txtLinkDir.Focus();
+                return;
+            }
+
+            if (!Directory.Exists(targetDir))
+            {
+                CustomMessageBox messageBox = new CustomMessageBox("Please input a target directory with a valid path.");
+                await DialogHost.Show(messageBox);
+                txtTargetDir.Focus();
+                return;
+            }
+
+            //the real work starts here
+            DirectoryInfo diLink = new DirectoryInfo(Path.Combine(linkDir, linkName));
+            DirectoryInfo diTarget = new DirectoryInfo(targetDir);
+
+            //lets see if there already exists a folder or symbolic link with that name
+            if (diLink.Exists == true)
+            {
+                //is it a symbolic link?
+                FileAttributes fa = File.GetAttributes(diLink.FullName);
+                if ((fa & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint)
+                {
+                    //ask user what to do? redirect the link or cancel operation
+                    CustomMessageBox messageBox = new CustomMessageBox("Symbolic Link with same name exists. \nShall I modify it to point to the new target?", "Ok", "Cancel");
+                    string result = await DialogHost.Show(messageBox) as string;
+
+                    //if redirect, delete the link
+                    if (result.Equals("1", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        try
+                        {
+                            diLink.Delete();
+                        }
+                        catch
+                        {
+                            CustomMessageBox messageBox2 = new CustomMessageBox("Could not redirect the symbolic link!\nCancelling operations.");
+                            await DialogHost.Show(messageBox2);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        txtLinkName.Focus();
+                        return;
+                    }
+                }
+                else
+                {
+                    //if it is a folder
+                    //check if there are contents in it
+                    FileInfo[] fiTest = diLink.GetFiles();
+                    DirectoryInfo[] diTest = diLink.GetDirectories();
+
+                    //if it is not empty
+                    if (fiTest.Length > 0 || diTest.Length > 0)
+                    {
+                        CustomMessageBox msgBox = new CustomMessageBox("There is a folder with the same name as the Symbolic Link. \nShall I move all the files in it to Target folder\nand convert the folder into a Symbolic Link?\nNote: files with the same name will be overwritten without prompt.", "Yes", "No", "Cancel");
+                        string result = await DialogHost.Show(msgBox) as string;
+
+                        if (result.Equals("1", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            //check if the target folder exists
+
+                            if (diTarget.Exists == false)
+                            {
+                                //if not, create
+                                try
+                                {
+                                    diTarget.Create();
+                                }
+                                catch
+                                {
+                                    CustomMessageBox dlg1 = new CustomMessageBox("Could not create target directory!");
+                                    await DialogHost.Show(dlg1);
+                                    return;
+                                }
+                            }
+
+                            //then copy the data in the link to target
+                            //Now Create all of the directories
+                            try
+                            {
+                                foreach (string dirPath in Directory.GetDirectories(diLink.FullName, "*", SearchOption.AllDirectories))
+                                {
+                                    Directory.CreateDirectory(dirPath.Replace(diLink.FullName, diTarget.FullName));
+                                }
+
+                                //Copy all the files
+                                foreach (string newPath in Directory.GetFiles(diLink.FullName, "*.*", SearchOption.AllDirectories))
+                                {
+                                    File.Copy(newPath, newPath.Replace(diLink.FullName, diTarget.FullName), true);
+                                }
+                            }
+                            catch
+                            {
+                                CustomMessageBox dlg2 = new CustomMessageBox("Could not copy contents of the source to the target directory!\nBut the data is still safe in the source directory.");
+                                await DialogHost.Show(dlg2);
+                                return;
+                            }
+
+                            try
+                            {
+                                diLink.Delete(true);
+                            }
+                            catch
+                            {
+                                CustomMessageBox dlg3 = new CustomMessageBox("Could not delete the source folder.\nBut the data is safe in the target folder.");
+                                await DialogHost.Show(dlg3);
+                                return;
+                            }
+                        }
+                        else if (result.Equals("2", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            try
+                            {
+                                diLink.Delete(true);
+                            }
+                            catch
+                            {
+                                CustomMessageBox dlg4 = new CustomMessageBox("Could not delete the source folder.");
+                                await DialogHost.Show(dlg4);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            diLink.Delete(true);
+                        }
+                        catch
+                        {
+                            CustomMessageBox dlg2 = new CustomMessageBox("Could not delete the source folder.");
+                            await DialogHost.Show(dlg2);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (diTarget.Exists == false)
+            {
+                //if not, create
+                try
+                {
+                    diTarget.Create();
+                }
+                catch
+                {
+                    CustomMessageBox dlg1 = new CustomMessageBox("Could not create target directory!");
+                    await DialogHost.Show(dlg1);
+                    return;
+                }
+            }
+
+            //create the process info
+            string args = "/c mklink /d \"" + diLink.FullName + "\" \"" + diTarget.FullName + "\"";
+            ProcessStartInfo pso = new ProcessStartInfo("cmd.exe", args);
+            //pso.Verb = "runas";
+
+            try
+            {
+                //create the symbolic link
+                Process.Start(pso);
+            }
+            catch
+            {
+                CustomMessageBox dlg2 = new CustomMessageBox("Could not create symbolic link!");
+                await DialogHost.Show(dlg2);
+                return;
+            }
+
+            CustomMessageBox dlg = new CustomMessageBox("Done!");
+            await DialogHost.Show(dlg);
+            return;
         }
 
-        private void btnClearLinkName_Click(object sender, RoutedEventArgs e)
-        {
-            txtLinkName.Clear();
-            txtLinkName.Focus();
-        }
+        #endregion Methods
 
-        private void btnBrowseTargetDir_Click(object sender, RoutedEventArgs e)
-        {
-        }
+        #region Main Menu
 
-        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        private void miHelpAbout_Click(object sender, RoutedEventArgs e)
         {
+            AboutWindow window = new AboutWindow();
+            window.Show();
         }
 
         private void FileExitCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -57,5 +269,55 @@ namespace SymbolicLinkMaker.Wpf
         {
             e.CanExecute = true;
         }
+
+        #endregion Main Menu
+
+        #region Buttons
+
+        private void btnBrowseLinkDir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenDialogViewModel vm = BrowseFolder();
+            bool? result = vm.Show();
+
+            if (result == true)
+            {
+                DirectoryInfo diLinkDir = new DirectoryInfo(vm.SelectedFilePath);
+                if (chkLink.IsChecked == true)
+                {
+                    txtLinkDir.Text = diLinkDir.Parent.FullName;
+                    txtLinkName.Text = diLinkDir.Name;
+                }
+                else
+                {
+                    txtLinkDir.Text = diLinkDir.FullName;
+                    txtLinkName.Text = "";
+                }
+            }
+        }
+
+        private void btnClearLinkName_Click(object sender, RoutedEventArgs e)
+        {
+            txtLinkName.Clear();
+            txtLinkName.Focus();
+        }
+
+        private void btnBrowseTargetDir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenDialogViewModel vm = BrowseFolder();
+            bool? result = vm.Show();
+
+            if (result == true)
+            {
+                DirectoryInfo diTargetDir = new DirectoryInfo(vm.SelectedFilePath);
+                txtTargetDir.Text = diTargetDir.FullName;
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            CreateSymLink(txtLinkDir.Text, txtLinkName.Text, txtTargetDir.Text);
+        }
+
+        #endregion Buttons
     }
 }
